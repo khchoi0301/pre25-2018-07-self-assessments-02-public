c63cd3cbab50c06b2fde1068691baa5a4e4528ec
kihong choi <khchoi0301@gmail.com>
2018-09-01 19:19:41 +0900

    Complete memoize

--- underbar-memoize/underbar-memoize.js
+++ underbar-memoize/underbar-memoize.js
@@ -1,6 +1,3 @@
-var _ = {};
-
-(function() {
 
   // Memorize an expensive function's results by storing them. You may assume
   // that the function takes only one argument and that it is a primitive.
@@ -10,7 +7,29 @@ var _ = {};
   // _.memoize should return a function that, when called, will check if it has
   // already computed the result for the given argument and return that value
   // instead if possible.
+
+var _ = {};
+
+(function() {
+
   _.memoize = function(func) {
+    var cache = {}
+
+    return function(){
+      var arg = Array.prototype.slice.call(arguments)
+      var stringArg = JSON.stringify(arg)
+      //console.log(arg,stringArg);
+
+      if(!(stringArg in cache)){
+        console.log('func called')
+        var result = func.apply(this,arg);
+        cache[stringArg]=result;          
+        return result;
+      } else {
+        return cache[stringArg]
+      }    
+    }
+
   };
 
 }).call(this);
