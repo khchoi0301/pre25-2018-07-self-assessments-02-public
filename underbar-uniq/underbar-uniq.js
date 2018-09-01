29066a3f073482e5001de4fba17e2a61391a61c7
kihong choi <khchoi0301@gmail.com>
2018-09-01 15:21:41 +0900

    Complete underbar-uniq

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -2,9 +2,17 @@ var _ = {};
 
 (function() {
 
-
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    
+    var newArr=[]
+    for(var i=0;i<array.length;i++){
+      if(!(newArr.includes(array[i]))){
+        newArr.push(array[i])
+      }
+    }
+//    console.log(newArr);
+    return newArr
   };
 
 }).call(this);
