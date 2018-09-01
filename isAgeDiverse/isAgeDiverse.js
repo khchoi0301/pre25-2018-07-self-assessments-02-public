a1df1ad2f66d443ee49396388e7a2b22f6de29e2
kihong choi <khchoi0301@gmail.com>
2018-09-01 15:00:45 +0900

    Complete isAgeDiverse

--- isAgeDiverse/isAgeDiverse.js
+++ isAgeDiverse/isAgeDiverse.js
@@ -9,20 +9,20 @@
 // false otherwise.
 
 // For example, given the following input array:
-
-// var list1 = [
-//   { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' },
-//   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
-//   { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
-//   { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
-//   { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
-//   { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
-//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
-//   { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
-//   { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
-//   { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' }
-// ];
-
+/*
+ var list1 = [
+   { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' },
+   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+   { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+   { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
+   { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
+   { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
+   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
+   { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
+   { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
+   { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' }
+ ];
+*/
 // your function should return true as there is at least one developer from each age group.
 
 // Notes:
@@ -32,5 +32,25 @@
 // Age is represented by a number which can be any positive integer up to 199.
 
 var isAgeDiverse = function(s) {
-  //Your code here
+  var ageArr = []
+  for(var i=0;i<s.length;i++){
+    if(s[i].age<100){
+    ageArr.push(Math.floor(s[i].age/10));
+    //console.log(s[i]['firstName'],s[i].age)
+    } else if(s[i].age>=100||ageArr[0]!==0) {
+      //console.log(s[i]['firstName'],s[i].age,'???')
+      ageArr.unshift(Math.floor(0));
+    }
+  }
+  ageArr.sort()
+  //console.log(ageArr)
+  var result = false
+  for(i=0;i<ageArr.length;i++){
+    if(ageArr[i]===i){
+      result = true
+    } else {
+    result = false
+    }
+  }
+  return result;
 };
