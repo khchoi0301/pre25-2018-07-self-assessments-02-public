9a7721135bdd60db486e9c7ebe01bca7b01289a5
kihong choi <khchoi0301@gmail.com>
2018-09-01 15:07:59 +0900

    Complete underbar-map

--- underbar-map/underbar-map.js
+++ underbar-map/underbar-map.js
@@ -2,12 +2,17 @@ var _ = {};
 
 (function() {
 
-
+  var newArr=[]
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
-    // map() is a useful primitive iteration function that works a lot
-    // like each(), but in addition to running the operation on all
-    // the members, it also maintains an array of results.
+    for(var i=0;i<collection.length;i++){
+      newArr.push(iterator(collection[i]))
+    }
+    return newArr
   };
 
 }).call(this);
+
+    // map() is a useful primitive iteration function that works a lot
+    // like each(), but in addition to running the operation on all
+    // the members, it also maintains an array of results.
