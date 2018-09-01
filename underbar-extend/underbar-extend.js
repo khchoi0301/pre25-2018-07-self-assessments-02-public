713d1bbbedc8fe35824d49aaac787ae7b1c9ed6d
kihong choi <khchoi0301@gmail.com>
2018-09-01 15:03:09 +0900

    Complete underbar-extend

--- underbar-extend/underbar-extend.js
+++ underbar-extend/underbar-extend.js
@@ -1,20 +1,31 @@
-var _ = {};
-
-(function() {
-
 
   // Extend a given object with all the properties of the passed in
   // object(s).
   //
   // Example:
-  //   var obj1 = {key1: "something"};
-  //   _.extend(obj1, {
-  //     key2: "something new",
-  //     key3: "something else new"
-  //   }, {
-  //     bla: "even more stuff"
-  //   }); // obj1 now contains key1, key2, key3 and bla
+    var obj1 = {key1: "something"};
+     _.extend(obj1, {
+      key2: "something new",
+      key3: "something else new"
+    }, {
+      bla: "even more stuff"
+    }); // obj1 now contains key1, key2, key3 and bla
+
+var _ = {};
+
+(function() {
+
   _.extend = function(obj) {
+      var arr=Array.prototype.slice(arguments)
+      var newObj={}
+      //console.log(arr,obj,arguments)
+
+      for(var i=0; i<arguments.length;i++){
+        for(var key in arguments[i]){
+          newObj[key]=arguments[i][key]
+        }
+      }
+      return newObj
   };
 
 }).call(this);
